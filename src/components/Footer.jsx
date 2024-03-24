8624f3521d31d49144191fb6ba09bffc7751a044
Mona Lisa <147671781+ajcasablanca@users.noreply.github.com>
2024-03-24 10:56:56 -0400

    basic header and footer created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+
+
+function Footer() {
+ return (
+    <footer class="contact-section">
+        <h3 id="Contact-Me" class="contact-title">Contact Me</h3>
+        <div class="contact-border">
+            <ul>
+                <li>
+                    <a href="https://github.com/ajcasablanca" target="_blank">GitHub</a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/andrew.casablanca/" target="_blank">Facebook</a>
+                </li>
+                <li>
+                    <a href="mailto:andrewcasablanca@gmail.com" target="_blank">andrewcasablanca@gmail.com</a>
+                </li>
+            </ul>
+        </div>
+
+    </footer>
+  ) 
+ 
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+
+
+function Header() {
+    return (
+                <header class="header">
+        <h1 class="header h1">Andrew Casablanca</h1>
+        <nav class="header nav">
+            <ul>
+                <li>
+                    <a href="#About-Me"> About Me</a>
+                </li>
+                <li>
+                    <a href="#Work"> Work</a>
+                </li>
+                <li>
+                    <a href="#Resume"> Resume</a>
+                </li>
+                <li>
+                    <a href="#Contact-Me"> Contact Me</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+       
+     ) 
+    
+   }
+   
+   export default Header;
